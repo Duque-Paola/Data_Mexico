71318973affbbe10c12d4a73f009fe1afcefbdf6
Nacho <ignaciodavidvazquezperez@gmail.com>
2020-12-03 05:25:56 -0600

    notebooks

--- actividad_economica.py
+++ actividad_economica.py
@@ -17,4 +17,46 @@ import matplotlib.ticker as ticker
 #%%
 path = "/home/nacho/Documents/coronavirus/Data_Mexico"
 os.chdir(os.path.join(path)) 
-violencia_og = pd.read_csv("data/IDEFC_NM_oct2020.csv", encoding='utf-8')
+act_econ_og = pd.read_csv("data/Actividad_Economica_Estatal.csv", encoding='utf-8')
+#%%ACT ECON Mexico
+act_econ_mx_2020 = act_econ_og.loc[2,['Unnamed: 83','Unnamed: 84','2020','Unnamed: 87']]
+act_econ_mx_2020.index =  ['t3','t4','t1','t2']
+
+act_econ_mx_2019 = act_econ_og.loc[2,['Unnamed: 78','Unnamed: 79','2019','Unnamed: 82']]
+act_econ_mx_2019.index =  ['t3','t4','t1','t2']
+
+#plot
+fig, ax = plt.subplots()
+ax.plot(act_econ_mx_2020.index, act_econ_mx_2020, label="2019-2020")
+ax.plot(act_econ_mx_2019.index, act_econ_mx_2019, label="2018-2019")
+ax.set_ylabel("Índice de volumen físico ")
+ax.set_xlabel("Trimestres")
+ax.set_title("Actividad Economica en México")
+plt.xticks(rotation='vertical')
+ax.legend()
+
+#porcentaje
+print("Total Mexico 2020: ", act_econ_mx_2020.astype(float).sum())
+print("Total Mexico 2019: ", act_econ_mx_2019.astype(float).sum())
+print("Porcentaje de incremento de Mexico:", act_econ_mx_2020.astype(float).sum()/act_econ_mx_2019.astype(float).sum()*100-100)
+#%%ACT ECON JALISCO
+act_econ_jal_2020 = act_econ_og.loc[16,['Unnamed: 83','Unnamed: 84','2020','Unnamed: 87']]
+act_econ_jal_2020.index =  ['t3','t4','t1','t2']
+
+act_econ_jal_2019 = act_econ_og.loc[16,['Unnamed: 78','Unnamed: 79','2019','Unnamed: 82']]
+act_econ_jal_2019.index =  ['t3','t4','t1','t2']
+
+#plot
+fig, ax = plt.subplots()
+ax.plot(act_econ_jal_2020.index, act_econ_jal_2020, label="2019-2020")
+ax.plot(act_econ_jal_2019.index, act_econ_jal_2019, label="2018-2019")
+ax.set_ylabel("Índice de volumen físico ")
+ax.set_xlabel("Trimestres")
+ax.set_title("Actividad Economica en Jalisco")
+plt.xticks(rotation='vertical')
+ax.legend()
+
+#porcentaje
+print("Total Jalisco 2020: ", act_econ_jal_2020.astype(float).sum())
+print("Total Jalisco 2019: ", act_econ_jal_2019.astype(float).sum())
+print("Porcentaje de incremento de Jalisco:", act_econ_jal_2020.astype(float).sum()/act_econ_jal_2019.astype(float).sum()*100-100)
--- desocupacion_data.py
+++ desocupacion_data.py
@@ -6,17 +6,59 @@ Este es un archivo temporal.
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-datos=pd.read_csv('Desocupacion.csv',header=0)
+from matplotlib.pyplot import *
+import os
+#%%
+path = "/home/nacho/Documents/coronavirus/Data_Mexico"
+os.chdir(os.path.join(path)) 
+datos = pd.read_csv("data/Desocupacion.csv",header=0)
+#datos=pd.read_csv('Desocupacion.csv',header=0)
 print(datos)
+#%%JALISCO
 newJalisco = (datos['Jalisco'])
-indices_2019 = (newJalisco.loc[7:10])
+indices_2019 = newJalisco.loc[[7,8,9,11]]
 print(indices_2019)
-indices_2020 = (newJalisco.loc[11:14])
+indices_2020 = (newJalisco.loc[[12,13,14,16]])
 print(indices_2020)
-#Promedio, desviacion estandar, maximo y minimo.
-#indices_2019.describe()
-#indices_2020.describe()
 
-newJalisco["Jalisco"].plot()
+index = ["T2","T3","T4","T1"]
+
+indices_2019.index = index
+indices_2020.index = index
+
+#plot
+fig, ax = subplots()
+indices_2019.plot()
+indices_2020.plot()
+ax.legend(["2018-2019", "2019-2020"])
+ax.set_title("Tasa de Desocupación en Jalisco")
 plt.show()
 
+#porcentaje
+print("Total Jalisco 2020: ", indices_2020.astype(float).sum())
+print("Total Jalisco 2019: ", indices_2019.astype(float).sum())
+print("Porcentaje de incremento de Jalisco:", indices_2020.astype(float).sum()/indices_2019.astype(float).sum()*100-100)
+#%%NACIONAL
+datos_mx = pd.read_csv("data/desocupacion_nacional_oct_2020.csv",header=0)
+
+datos_mx_2020 = datos_mx['“tasa_desocupacion”'].iloc[180:190,]
+
+datos_mx_2019 = datos_mx['“tasa_desocupacion”'].iloc[168:178,]
+
+index_month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre']
+
+datos_mx_2020.index = index_month
+datos_mx_2019.index = index_month
+
+#plot
+fig, ax = subplots()
+datos_mx_2019.plot()
+datos_mx_2020.plot()
+ax.legend(["2019", "2020"])
+ax.set_title("Tasa de Desocupación en Mexico")
+plt.show()
+
+#porcentaje
+print("Total Mexico 2020: ", datos_mx_2020.astype(float).sum())
+print("Total Mexico 2019: ", datos_mx_2019.astype(float).sum())
+print("Porcentaje de incremento de Mexico:", datos_mx_2020.astype(float).sum()/datos_mx_2019.astype(float).sum()*100-100)
